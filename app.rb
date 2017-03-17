6805fde93d8a077133d4f26e55600549a088521e
Lilly Piri <lilly@lillypiri.com>
2017-03-17 10:26:54 +1000

    Edited comments

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 # In Ruby everything is an object. Classes are a way of describing objects.
 # A class has a constructor which is called 'initialize'.
-#When you call Class.new (that is the name of the Class and then .new),
+# When you call Class.new (that is the name of the Class and then .new),
 # it runs the constructor. The constructor sets up the new object of type class.
 # Variables have a type. The basic types are: number, string, boolean (true or false).
 
@@ -16,7 +16,7 @@ class Animal
 end
 
 # Running this app in terminal will spit out "Socks: meow
-#Buddy: woof"
+# Buddy: woof"
 # In Animal Class above, 'name' will only work in Animal class and subclasses
 # (Cat and Dog) because they inherit it. The '<' means it extends the class.
 # So "Cat extends Animal".
